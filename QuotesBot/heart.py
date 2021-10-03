b7328f29f5e95c4e32ddc1679f605ba99a0e82a2
Crazy devil <85053195+kkverma25@users.noreply.github.com>
2021-10-03 14:39:33 +0530

    Update heart.py

--- QuotesBot/heart.py
+++ QuotesBot/heart.py
@@ -31,13 +31,13 @@ async def brainy_quotes(query):
         result = InlineQueryResultArticle(
             title=author,
             input_message_content=InputTextMessageContent("ıllıllı★ 𝐁𝐫𝐚𝐢𝐧𝐲 𝐐𝐮𝐨𝐭𝐞𝐬 𝐁𝐨𝐭 ★ıllıllı \n\n" + quote + "\n\n~ " + author),
-            url="https://t.me/StarkBots",
+            url="https://t.me/ABOUT_DEVIL_DAD/4",
             description=quote,
             thumb_url="https://telegra.ph/file/9fd2796d73782364dd2df.jpg",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("✨ Search More Quotes ✨", switch_inline_query_current_chat="")],
-                    [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")]
+                    [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/ABOUT_DEVIL_DAD/4")]
                 ]
             ),
         )
@@ -50,13 +50,13 @@ async def brainy_quotes(query):
 main = InlineQueryResultArticle(
             title="Brainy Quotes Bot",
             input_message_content=InputTextMessageContent(Data.HELP),
-            url="https://t.me/StarkBots",
+            url="https://t.me/ABOUT_DEVIL_DAD/4",
             description="Learn How to use me efficiently",
             thumb_url="https://telegra.ph/file/9fd2796d73782364dd2df.jpg",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [InlineKeyboardButton("✨ Search Quotes ✨", switch_inline_query_current_chat="")],
-                    [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")]
+                    [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/ABOUT_DEVIL_DAD/4")]
                 ]
             ),
         )
